cb52b71deeb8685826358b81f47c9426e60412b8
jahid <jahidbinkibria@gmail.com>
2021-01-25 20:43:22 +0600

    Updates

---
+++ precache-manifest.be990b90c1255167cf4aab2422e6ccc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0f084bd1e7936fa5aa1117600c4e08",
+    "url": "/diabetes_disease_prediction/index.html"
+  },
+  {
+    "revision": "c0b7a7c8a0093b3a4dcc",
+    "url": "/diabetes_disease_prediction/static/css/main.8fcd3e8b.chunk.css"
+  },
+  {
+    "revision": "2a8501ca5550d5a5a747",
+    "url": "/diabetes_disease_prediction/static/js/2.d85fe244.chunk.js"
+  },
+  {
+    "revision": "97ce0d5e601e72a0c90275d966d6f16b",
+    "url": "/diabetes_disease_prediction/static/js/2.d85fe244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b7a7c8a0093b3a4dcc",
+    "url": "/diabetes_disease_prediction/static/js/main.83423624.chunk.js"
+  },
+  {
+    "revision": "a1c1ead277cdb8a04213",
+    "url": "/diabetes_disease_prediction/static/js/runtime-main.4b9fa54d.js"
+  }
+]);
